665eca67ba9dffc92c7e9fe9cd6c6ae5f27b0306
Shazid <Milgate#313>
2025-01-26 16:24:35 +0600

    rotate function fixed

--- rotate.py
+++ rotate.py
@@ -1,29 +1,17 @@
 import RPi.GPIO as GPIO
 import time
-
+from gpiozero import Servo
+servo = Servo(21)  # GPIO pin connected to the servo
 # Function to rotate servo 360° clockwise for 0.2 seconds
 def rotate_360_clockwise():
-    servo_pin = 21  # GPIO pin connected to servo
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(servo_pin, GPIO.OUT)
-    pwm = GPIO.PWM(servo_pin, 50)  # 50Hz frequency
-    pwm.start(0)  # Start with a duty cycle of 0
-    pwm.ChangeDutyCycle(10)  # Adjust duty cycle for clockwise rotation
-    time.sleep(0.3)  # Rotate for 0.2 seconds
-    pwm.ChangeDutyCycle(0)  # Stop servo
-    pwm.stop()
-    GPIO.cleanup()  # Clean up GPIO settings
+    servo.value = 1  # Full clockwise rotation
+    time.sleep(.3)
+    servo.value = 0  # Stop the servo
 
 
 # Function to rotate servo 360° counterclockwise for 0.2 seconds
 def rotate_360_counterclockwise():
-    servo_pin = 21  # GPIO pin connected to servo
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(servo_pin, GPIO.OUT)
-    pwm = GPIO.PWM(servo_pin, 50)  # 50Hz frequency
-    pwm.start(0)  # Start with a duty cycle of 0
-    pwm.ChangeDutyCycle(5)  # Adjust duty cycle for counterclockwise rotation
-    time.sleep(0.25)  # Rotate for 0.2 seconds
-    pwm.ChangeDutyCycle(0)  # Stop servo
-    pwm.stop()
-    GPIO.cleanup()  # Clean up GPIO settings
+      # Clean up GPIO settings
+    servo.value = -1  # Full counterclockwise rotation
+    time.sleep(.3)
+    servo.value = 0 
